5ff8626be6bc63ced2a0389ac0ede0d6003c861e
Christian Calle C <chcallec@gmail.com>
2022-12-25 19:04:26 +0100

    fixed h2 as question not questions

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import Form from './Form';
-import Loading from './Loading';
+import React from "react";
+import { useGlobalContext } from "./context";
+import Form from "./Form";
+import Loading from "./Loading";
 
 const Main = () => {
-    const {waiting, isLoading, questions, index, correct} = useGlobalContext();
-    if(waiting){
-        return <Form />
-    }
-    if(isLoading){
-        return <Loading />
-    }
-    const {question, incorrect_answers, correct_answer} = (questions[index])
-    const answers = [...incorrect_answers, correct_answer] 
+  const { waiting, isLoading, questions, index, correct } = useGlobalContext();
+  if (waiting) {
+    return <Form />;
+  }
+  if (isLoading) {
+    return <Loading />;
+  }
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
   return (
     <main>
-        <section className='quiz'>
-            <p className='correct-answer'>
-                correct answers: {correct} / {index}
-            </p>
-        </section>
+      <section className="quiz">
+        <p className="correct-answer">
+          correct answers: {correct} / {index}
+        </p>
+        <div className="container">
+          <h2>{question}</h2>
+          <div>
+            {answers.map((answer, index) => (
+              <button key={index} className="answer-btn">
+                {answer}
+              </button>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
