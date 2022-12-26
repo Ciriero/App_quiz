e93bacf3cc9213c9d78b10b0f1c5d99cf747c004
Christian Calle C <chcallec@gmail.com>
2022-12-26 18:12:09 +0100

    form.js: setup with global context

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Form = () => {
+    const {values, handleChange, handleSubmit, error} = useGlobalContext()
   return (
     <div>Form</div>
   )
