39e24b1450a8d14b1632209e1ced783cfff5b42c
Christian Calle C <chcallec@gmail.com>
2022-12-26 18:35:02 +0100

    form.js and index.css: added form and styles

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,66 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Form = () => {
-    const {values, handleChange, handleSubmit, error} = useGlobalContext()
+  const { values, handleChange, handleSubmit, error } = useGlobalContext();
   return (
-    <div>Form</div>
-  )
-}
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+            <h3>App Quiz</h3>
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={values.amount}
+              onChange={handleChange}
+              className="form-input"
+              min={1}
+              max={50}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">category</label>
+            <select
+              name="category"
+              id="category"
+              className="form-input"
+              value={values.category}
+              onChange={handleChange}
+            >
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="difficulty">select difficulty</label>
+            <select
+              name="difficulty"
+              id="difficulty"
+              className="form-input"
+              value={values.difficulty}
+              onChange={handleChange}
+            >
+              <option value="easy">easy</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className="error">
+              Can't generate questions, please try different options
+            </p>
+          )}
+          <button type="submit" onClick={handleSubmit} className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
-export default Form
+export default Form;
